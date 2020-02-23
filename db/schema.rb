7a1ecff6bdb34c9576cb9bce60105ea9e46aeaf7
nemoto <auther.masahito@gmail.com>
2020-02-23 17:11:17 +0900

    Memoモデルを作成し、トップページにメモを表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200223074834) do
+
+  create_table "memos", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
