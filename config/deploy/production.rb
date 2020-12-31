5e40bf64979b825487088c6a17ac37d4229a943a
nemotomasafuminoMacBook-Pro <auther.masahito@gmail.com>
2021-01-01 04:13:54 +0900

    一旦コミット

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,11 @@
-server '54.150.34.66', user: 'ec2-user', roles: %w{app db web}
+set :stage, :production
+set :rails_env, 'production'
+server '54.150.34.66', user: 'ec2-user',
+roles: %w{web app db}
+set :ssh_options, {
+ keys: [File.expand_path('~/.ssh/id_rsa.pem)')]
+}
+
+
+
+
