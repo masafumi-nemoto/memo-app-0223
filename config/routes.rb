73e2c2bb830e501369e4f4bc8fadc8ad3817e958
nemotomasafuminoMacBook-Pro <auther.masahito@gmail.com>
2021-01-01 03:18:37 +0900

    デプロイのための修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
     get "/memos/:id/edit", to: "memos#edit"
     patch "/memos/:id", to: "memos#update"
     get "/categories/:id", to: "categories#show"
+    $date = Time.now.in_time_zone('Tokyo').to_s
+    root "articles#index"
+    resources :articles, only: :index
 end
